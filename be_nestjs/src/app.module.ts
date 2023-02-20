c565ab9d924c8761f4eaf1efb0732c3a6ca90c56
Emanuel Epifani <emanuel.epifani@ioma.ai>
2023-02-20 15:34:13 +0100

    fix configuration (later move data in a .env file extern)

--- be_nestjs/src/app.module.ts
+++ be_nestjs/src/app.module.ts
@@ -3,11 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudenteModule } from './studente/studente.module';
-//import { Studente } from './studente/entities/studente.entity';
 import { StudenteService } from './studente/studente.service';
 import { StudenteController } from './studente/studente.controller';
 import { Studente } from './studente/entities/studente.entity';
-import { DataSource } from 'typeorm';
 import {MongooseModule} from "@nestjs/mongoose";
 import {JwtModule} from "@nestjs/jwt";
 import {UserSchema} from "./auth/user/user.entity";
@@ -15,26 +13,22 @@ import {UserSchema} from "./auth/user/user.entity";
 @Module({
   imports: [
     //----------------   connection POSTGRES  -----------------
-    //
     TypeOrmModule.forRoot({
       type: 'postgres',
-      //host: '192.168.1.215',
       host: 'localhost',
       port: 5433,
       username: 'emanuelepifani',
       password: 'gigetto',
       database: 'emanuelepifani',
-      //entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       entities: [Studente],
       synchronize: false, //se a true, ogni volte che re-run ti resetta il db?
     }),
-    TypeOrmModule.forFeature([Studente]),
+    TypeOrmModule.forFeature([
+        Studente
+    ]),
     StudenteModule,
-    //----------------   connection MONGODB -----------------
-    // ($ npm i @nestjs/mongoose mongoose)
-    MongooseModule.forRoot(
-        'mongodb+srv://srv-staging-etable:tMzY8eD7YiHSdAKD@etable-cl.ftxyywv.mongodb.net/etable-staging?retryWrites=true&w=majority',
-    ),
+    //----------------   connection MONGODB ($ npm i @nestjs/mongoose mongoose) -----------------
+    MongooseModule.forRoot('mongodb+srv://srv-staging-etable:tMzY8eD7YiHSdAKD@etable-cl.ftxyywv.mongodb.net/etable-staging?retryWrites=true&w=majority'),
     MongooseModule.forFeature([
       { name: "User", schema: UserSchema }
     ]),
