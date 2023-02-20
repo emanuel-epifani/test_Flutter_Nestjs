4eb276aeb0ed906613bc9f400d9abf018a2569e3
Emanuel Epifani <88022929+emanuel-epifani@users.noreply.github.com>
2023-02-20 22:51:48 +0100

    add alternative "main()"

--- be_nestjs/src/app.service.ts
+++ be_nestjs/src/app.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap  } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
 
 
+   onApplicationBootstrap() {
+      console.log('Server avviato!');
+      // Qui puoi eseguire l'operazione che desideri,e testare funzioni/modifiche come fosse una main() function dei compilati
+    }
 
 
 
