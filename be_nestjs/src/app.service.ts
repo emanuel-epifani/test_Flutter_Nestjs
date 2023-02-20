f4ba02ee98327cfab91af4ab3dc11e5870bf6505
Emanuel Epifani <88022929+emanuel-epifani@users.noreply.github.com>
2023-02-20 22:53:44 +0100

    add completato

--- be_nestjs/src/app.service.ts
+++ be_nestjs/src/app.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, OnApplicationBootstrap  } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnApplicationBootstrap {
+//https://docs.nestjs.com/fundamentals/lifecycle-events
 
 
    onApplicationBootstrap() {
